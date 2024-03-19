7d19aa37454e7e6ab7c61a333800a18e6569b827
Pma <leejhan0719@naver.com>
2024-03-20 08:07:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
-import requests, os
+import requests
+import os
 from datetime import datetime
 from bs4 import BeautifulSoup
+
 header = {
-    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
 }
 
-def upload_gitAction(data : str):
-    token = os.environ.get('TOKEN')
-    url = "https://api.github.com/repos/Pma10/MelonChartTop100/issues"
-    response = requests.post(url,json={"title":f"{datetime.now().strftime('%Y년%m월%d일')} 멜론차트 TOP100","body":data},headers={"Authorization": f"token {token}"})
-    if response.status_code == 200:
+def upload_gitAction(data: str):
+    try:
+        token = os.environ.get('TOKEN')
+        url = "https://api.github.com/repos/Pma10/MelonChartTop100/issues"
+        response = requests.post(url, json={"title": f"{datetime.now().strftime('%Y년%m월%d일')} 멜론차트 TOP100", "body": data}, headers={"Authorization": f"token {token}"})
+        response.raise_for_status()  # Raise an exception for 4XX and 5XX status codes
         print('업로드 성공')
-    else:
-        print('업로드 실패')
+    except requests.exceptions.RequestException as e:
+        print('업로드 실패:', e)
+
 def get_melon_chart():
-    melon100 = requests.get("https://www.melon.com/chart/index.htm",headers=header).text
-    soup = BeautifulSoup(melon100,'html.parser')
-    titles = soup.select('div.ellipsis.rank01 > span')
-    authors = soup.select('div.ellipsis.rank02 > span')
-    return [title.text for title in titles], [author.text for author in authors]
+    try:
+        melon100 = requests.get("https://www.melon.com/chart/index.htm", headers=header).text
+        soup = BeautifulSoup(melon100, 'html.parser')
+        titles = soup.select('div.ellipsis.rank01 > span')
+        authors = soup.select('div.ellipsis.rank02 > span')
+        return [title.text for title in titles], [author.text for author in authors]
+    except requests.exceptions.RequestException as e:
+        print('멜론 차트 데이터를 가져오는 데 실패했습니다:', e)
+        return [], []
 
-
-with open(f'melon_chart/{datetime.now().strftime("%Y년%m월%d일")}.txt','a',encoding='utf-8') as f:
+with open(f'melon_chart/{datetime.now().strftime("%Y년%m월%d일")}.txt', 'a', encoding='utf-8') as f:
     titles, authors = get_melon_chart()
     for rank in range(len(titles)):
-        title_name = titles[rank].replace('\n','').strip()
+        title_name = titles[rank].replace('\n', '').strip()
         f.write(f"TOP {rank + 1} {title_name} - {authors[rank]}\n")
+
+upload_gitAction("\n".join([f"TOP {rank + 1} {title_name} - {authors[rank]}" for rank, (title_name, author) in enumerate(zip(titles, authors))]))
